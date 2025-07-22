22f204d6e88e28c8524709a1b3e7879c005ed991
Anna <173553840+abacaxi-seco@users.noreply.github.com>
2025-07-22 21:17:55 +0200

    update 1D poisson to include pseudo-transient solver

--- Poisson1D_v2.jl
+++ Poisson1D_v2.jl
@@ -1,14 +1,14 @@
 using Plots, LinearAlgebra
 
 function main()
-    k0 = 1.0
-    σ  = Lx /10
-
     Lx = 1.0
     ncx = 200
     Δx = Lx / ncx
     xce= LinRange(-Lx/2 - Δx/2, Lx/2 + Δx/2, ncx+2)
     xv = LinRange(-Lx/2, Lx/2, ncx+1)
+
+    k0 = 1.0
+    σ  = Lx /10
 
     BC = (w = 1.0, e = 2.0)
     
@@ -17,20 +17,31 @@ function main()
     q = zeros(ncx+1)
     k = k0 * ones(ncx+1)
     r = zeros(ncx)
+    r0 = zeros(ncx)
     b = zeros(ncx)
     ∂u∂τ = zeros(ncx)
 
     # Initialize fields
     b  .= 5 * exp.(-xce[2:end-1].^2 / σ^2)
-    k  .= 5 * exp.(-xv.^2 / σ^2)
+    # k  .= 1 * exp.(-xv.^2 / σ^2)
 
     # Iteration parameters
-    Δτ    = Δx^2 /k0 / 5    # pseudo time step
-    θ     = 0.001
+    CFL   = 0.9                     # Courant-Friedrichs-Levi criterium
+    cfact = 0.5
+    λmax  = 4 * maximum(k) / Δx^2   # maximum eigenvalue
+    λmin  = 0.0                     # minimum eigenvalue    
+    Δτ = 2/sqrt(λmax) * CFL         # pseudo time step
+    c = 2 * sqrt(λmin) * cfact
+    α = (2 - c * Δτ) / (2 + c * Δτ)
+    β = 2 * Δτ / (2 + c * Δτ)
+
     niter = 2e5
     ϵ     = 1e-6
 
     for iter = 1:niter
+        # store previous residual
+        r0 .= r
+
         # set BC
         u[1]   = BC.w * 2  - u[2]
         u[end] = BC.e * 2 - u[end-1]
@@ -42,7 +53,7 @@ function main()
         r .= - diff(q)/Δx .+ b
 
         # update ∂u∂τ
-        ∂u∂τ .= r .+ (1-θ) .* ∂u∂τ
+        ∂u∂τ .= β .* r .+ α .* ∂u∂τ
 
         # update u
         u[2:end-1] .+= Δτ .* ∂u∂τ
@@ -51,12 +62,20 @@ function main()
             nr = norm(r)
             println(nr)
             if isnan(nr)
-                error("some error")
+                error("norm(r) = NaN")
             end
             if nr < ϵ
                 println(iter)
                 break
             end
+
+            #  Pseudo-Transient parameters
+            λmax  = 4 * maximum(k) / Δx^2
+            λmin = abs.(((r .- r0)' * (∂u∂τ .* Δτ)) / ((∂u∂τ .* Δτ)' * (∂u∂τ .* Δτ)))
+            Δτ = 2/sqrt(λmax) * CFL
+            c = 2 * sqrt(λmin) * cfact
+            α = (2 - c * Δτ) / (2 + c * Δτ)
+            β = 2 * Δτ / (2 + c * Δτ)
         end
     end
 
