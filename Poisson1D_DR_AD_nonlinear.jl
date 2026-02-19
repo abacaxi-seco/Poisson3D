25a8584006faa71879067fdf8345ef7f74b89f20
Anna <173553840+abacaxi-seco@users.noreply.github.com>
2026-02-19 19:23:00 +0100

    nonlinear Poisson kind of works

--- Poisson1D_DR_AD_nonlinear.jl
+++ Poisson1D_DR_AD_nonlinear.jl
@@ -1,16 +1,25 @@
 using Plots, LinearAlgebra, StaticArrays
 using Enzyme
-function avg(x,y)
+function av(x,y)
     return 0.5 * (x + y)
 end
 
+conductivity(abs_u, k0, a, n) = k0 + a * abs_u^n 
+
 function compute_residual_local(u, b, BC, Δx, k, len_u, i)
     uC = u[1]
-    uW = i== 2    ? BC.w * 2  - uC : u[2]
-    uE = i== len_u[1]-1 ? BC.e * 2 - uC : u[3]
+    uW = i== 2          ? (2*BC.w - uC) : u[2]
+    uE = i== len_u[1]-1 ? (2*BC.e - uC) : u[3]
 
-    kW = avg(uC, uW)^3
-    kE = avg(uC, uE)^3
+    k0, a, n = 1.0, 5e-3, 3
+    kW = k[i-1] = conductivity(av(abs(uC), abs(uW)), k0, a, n)
+    kE = k[i]   = conductivity(av(abs(uC), abs(uE)), k0, a, n)
+
+    if kW <= 0 || kE <= 0 
+        error("Negative diffusivity: kW = $kW, kE = $kE at i = $i, uC = $uC, uW = $uW, uE = $uE, u = $u")
+    elseif !isfinite(kW) || !isfinite(kE)
+        error("Non-finite diffusivity: kW = $kW, kE = $kE at i = $i, uC = $uC, uW = $uW, uE = $uE, u = $u")
+    end
 
     qW = -kW * (uC - uW) / Δx
     qE = -kE * (uE - uC) / Δx
@@ -23,16 +32,13 @@ function compute_residual(u, b, r0, BC, Δx, k, deriv, i)
     uW = u[i-1]
     uE = u[i+1]
     𝑢 = @SVector[uC,uW,uE]
-    kW = k[i-1]
-    kE = k[i]
-    k_l = @SVector[kW,kE]
     if deriv==true
-        ∂r∂u = gradient(Reverse, compute_residual_local, 𝑢, Const(b[i]), Const(BC), Const(Δx), Const(k_l), Const(size(u)), Const(i))
-        D = abs.(∂r∂u[1][1]) # diagonal entries of ∂r∂u
-        G = sum(abs.(∂r∂u[1]))
-        out = D, G
+        ∂r∂u = gradient(Reverse, compute_residual_local, 𝑢, Const(b[i]), Const(BC), Const(Δx), Const(k), Const(size(u)), Const(i))
+        D    = abs.(∂r∂u[1][1]) # diagonal entries of ∂r∂u
+        G    = sum(abs.(∂r∂u[1]))
+        out  = D, G
     else
-        r = compute_residual_local(𝑢, b[i], BC, Δx, k_l, size(u), i)
+        r   = compute_residual_local(𝑢, b[i], BC, Δx, k, size(u), i)
         out = r, r0[i]
     end
     return out
@@ -54,7 +60,7 @@ end
 
 function main()
     Lx  = 1.0
-    ncx = 20
+    ncx = 200
     nce = ncx + 2
     Δx  = Lx / ncx
     xce = LinRange(-Lx/2 - Δx/2, Lx/2 + Δx/2, ncx+2)
@@ -76,19 +82,19 @@ function main()
     G    = ones(nce)
     D    = ones(nce)
 
+    ∈
     # Initialize fields
     b  .= 5 * exp.(-xce.^2 / σ^2)
-    k  .= 1.0 .+ k0 * exp.(-xv.^2 / σ^2)
-    u  .= 1.0 .+ k0 * exp.(-xce.^2 / σ^2)
+    # k  .= 1.0 .+ k0 * exp.(-xv.^2 / σ^2)
+    # u  .= 1.0 .+ exp.(-xce.^2 / σ^2)
 
     # Evaluate G and D (Gershgorin)
     update_field(D, G, compute_residual, (u, b, r, BC, Δx, k, true))
 
     # Iteration parameters
-    CFL   = 0.98                                            # Courant-Friedrichs-Levi criterium
+    CFL   = 0.989                                           # Courant-Friedrichs-Levi criterium
     cfact = 0.5
-    # λmax  = maximum(2 * (k[2:end] .+ k[1:end-1]) / Δx^2 )   # maximum eigenvalue
-    λmax  = maximum(G ./ D)
+    λmax  = maximum(G ./ D)                                 # maximum eigenvalue
     λmin  = 0.0                                             # minimum eigenvalue
     Δτ    = 2/sqrt(λmax) * CFL                              # pseudo time step
     c     = 2 * sqrt(λmin) * cfact
@@ -106,6 +112,13 @@ function main()
         update_field(u, ∂u∂τ, update_u, (u, ∂u∂τ, r, Δτ, D, β, α))
 
         if iter == 1 || mod(iter,1e3) == 0
+            # Visualization
+            p1 = plot(xce[2:end-1], u[2:end-1], ylabel="u", xlabel="x", label=:none, title="Iteration $iter")
+            p2 = plot(xv, k, ylabel="k", xlabel="x", label=:none)
+            p3 = plot(xce, b, ylabel="b", xlabel="x", label=:none)
+            display(plot(p1,p2,p3))
+            sleep(0.1)
+
             nr = norm(r)
             println(nr)
             if isnan(nr)
@@ -128,8 +141,8 @@ function main()
         end
     end
     # Visualization
-    p1 = plot(xce[2:end-1], u[2:end-1], label="u", title="Poisson 1D with DR and AD")
-    p2 = plot(xce, b, label="b")
+    p1 = plot(xce[2:end-1], u[2:end-1], ylabel="u", xlabel="x", label=:none)
+    p2 = plot(xce, b, ylabel="b", xlabel="x", label=:none)
     display(plot(p1,p2))
     sleep(0.1)
 end
